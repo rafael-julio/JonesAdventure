d01a817dc658e5f5ca40e4714ee7af9a1052d522
rafael-julio <37933983+rafael-julio@users.noreply.github.com>
2018-06-21 10:25:37 -0300

    Add files via upload

---
+++ ControledeCamera.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControledeCamera : MonoBehaviour {
+
+	private Vector2 velocity;
+
+	public float delayX;
+	public float delayY;
+
+	public Transform player;
+
+
+	public bool bounds;
+
+	public Vector3 minCameraPos;
+	public Vector3 maxCamerPos;
+
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		float posX = Mathf.SmoothDamp (transform.position.x, player.position.x, ref velocity.x, delayX);
+		float posY = Mathf.SmoothDamp (transform.position.y, player.position.y, ref velocity.y, delayY);
+
+
+				transform.position = new Vector3 (posX, posY, transform.position.z);
+
+
+		if (bounds) {
+
+			transform.position = new Vector3 (
+
+				Mathf.Clamp(transform.position.x, minCameraPos.x, maxCamerPos.x),
+				Mathf.Clamp(transform.position.y, minCameraPos.y, maxCamerPos.y),
+				Mathf.Clamp(transform.position.z, minCameraPos.z, maxCamerPos.z)
+			
+			);
+	}
+	}
+}
+
+
---
+++ SoundEffectScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SoundEffectScript : MonoBehaviour {
+
+public static SoundEffectScript Instance;
+
+public AudioClip jumpingSound;
+public AudioClip enemySound;
+
+void Awake()
+{
+	if (Instance != null)
+	{
+		Debug.LogError("Existem múltiplas instâncias do script SoundEffectScript.");
+	}
+
+	Instance = this;
+}
+
+public void MakeJumpingSound()
+{
+		MakeSound (jumpingSound);
+}
+
+public void MakeEnemySound()
+{
+	MakeSound (enemySound);
+}
+
+private void MakeSound(AudioClip originalClip)
+{
+	AudioSource.PlayClipAtPoint (originalClip, transform.position);
+}
+}
